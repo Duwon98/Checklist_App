df07976cf142512b4f44df465de70ee6caca289a
Duwon Ha <duwon.ha@griffithuni.edu.au>
2022-03-18 12:30:38 +1000

    Initial Commit

---
+++ Milestone 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Milestone 1
+//
+//  Created by Duwon Ha on 18/3/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Milestone 1/Milestone_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone_1App.swift
+//  Milestone 1
+//
+//  Created by Duwon Ha on 18/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct Milestone_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
