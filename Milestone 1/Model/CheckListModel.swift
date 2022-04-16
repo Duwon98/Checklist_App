91ded4989dc3eaf191b9cc0a3d952c4c8b811072
Duwon Ha <duwon.ha@griffithuni.edu.au>
2022-04-16 11:52:39 +1000

    Added documentation in Model and view model

--- Milestone 1/Model/CheckListModel.swift
+++ Milestone 1/Model/CheckListModel.swift
@@ -8,11 +8,12 @@
 import Combine
 import Foundation
 
-
+// Model
 class CheckList: Identifiable, ObservableObject {
     @Published var title: String
+    //Checklist title
     @Published var lists = [String]()
-
+    //Checklist's list (it is array)
     init(title: String) {
         self.title = title
     }
--- Milestone 1/ViewModel/CheckListViewModel.swift
+++ Milestone 1/ViewModel/CheckListViewModel.swift
@@ -6,15 +6,20 @@
 //
 
 import Foundation
-
+//View Model
 class CheckListViewModle: Identifiable, ObservableObject {
     @Published var checklist: CheckList
+    // class Checklist
     @Published var tickList = [Bool]()
+    // each checklist have tickList array (False is unticked)
     @Published var previousTickList = [Bool]()
+    // To save previousTickList(undo function)
     var index = [Int]()
+    // To present the checklist's list
     var maxIndex = -1
+    // max index of checklist's array
 
-    
+    // To add list in checklist, it get title of the list
     func addList(name: String){
       self.maxIndex += 1
       self.index.append(self.maxIndex)
@@ -23,6 +28,7 @@ class CheckListViewModle: Identifiable, ObservableObject {
       self.previousTickList.append(false)
     }
     
+    // To delete list in checklist, it get position of the list
     func deleteList(position: Int){
         self.maxIndex -= 1
         self.index.removeLast()
@@ -30,21 +36,21 @@ class CheckListViewModle: Identifiable, ObservableObject {
         self.tickList.remove(at: position)
         self.previousTickList.remove(at: position)
     }
-    
+    // get position of the list, if it's false it will switch to true otherwise to false. False is unticked
     func tick(position: Int){
         self.tickList[position] = !(self.tickList[position])
     }
-    
+    // it will return tickList to save a current ticklist for Undo function
     func returnTickList() -> [Bool]{
         return tickList
     }
-    
+    // it will change all the values of tickList to false. which is unticked
     func removeTickList() {
         for i in 0 ..< tickList.count{
             self.tickList[i] = false
         }
     }
-    
+    //  it will switch current tickList to previousOne
     func updateUndo(previousOne: [Bool]){
         self.tickList = previousOne
     }
